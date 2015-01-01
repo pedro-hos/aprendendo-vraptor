33881fd9b1263fd663521097abc2d9426ea55ee8
pedro-hos <pedro-hos@outlook.com>
2015-01-01 14:47:14 -0200

    ola mundo completo

---
+++ ola-vraptor/src/main/java/br/com/pedroHos/controller/OlaController.java
@@ -0,0 +1,12 @@
+package br.com.pedroHos.controller;
+
+import br.com.caelum.vraptor.Controller;
+
+@Controller
+public class OlaController {
+	
+	public void digaOla() {
+		System.out.println("# Olá Vraptor Controller!");
+	}
+
+}
