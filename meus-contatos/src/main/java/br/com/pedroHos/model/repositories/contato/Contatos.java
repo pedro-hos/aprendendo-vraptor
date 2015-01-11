713808afc69dd1f78cf74d83e527088b17538dbe
pedro-hos <pedro-hos@outlook.com>
2015-01-11 20:45:44 -0200

    add repositorio

---
+++ meus-contatos/src/main/java/br/com/pedroHos/model/repositories/contato/Contatos.java
@@ -0,0 +1,9 @@
+package br.com.pedroHos.model.repositories.contato;
+
+import javax.ejb.Stateless;
+
+import br.com.pedroHos.model.entities.contato.Contato;
+import br.com.pedroHos.model.repositories.Repository;
+
+@Stateless
+public class Contatos extends Repository<Contato> {}
